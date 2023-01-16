61d703fb14723bf2c30ba483fa882d5d65a0c0c4
CrazyCoding330 <97684719+CodingLife004@users.noreply.github.com>
2023-01-16 08:37:06 -0600

    initial version

---
+++ myworld/python_api/members/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details')
+]
---
+++ myworld/python_api/members/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+
+    return HttpResponse(template.render(context, request))
